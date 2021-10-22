6a0a3d06a885899102ac94d7343741ed18cb7f5d
asl <3344570351@qq.com>
2021-10-22 13:29:49 +0800

    提交模板

---
+++ script/esbuild.options.ts
@@ -0,0 +1,35 @@
+import { join } from 'path';
+import { esbuildDecorators } from "@anatine/esbuild-decorators";
+import { builtinModules } from "module";
+import { BuildOptions } from "esbuild";
+import dotenv from 'dotenv';
+dotenv.config({ path: join(__dirname, '../.env') });
+
+const define = {};
+for (const k in process.env) {
+    if (['ProgramFiles(x86)', 'CommonProgramFiles(x86)'].includes(k)) continue;
+    define[`process.env.${k}`] = JSON.stringify(process.env[k]);
+}
+
+export function createOptions(): BuildOptions {
+    return {
+        entryPoints: [join(__dirname, '../src/main/index.ts')],
+        target: 'es2020',
+        //main所有ts文件都打包成了一个
+        outfile: join(__dirname, '../dist/main/index.js'),
+        format: 'cjs',
+        bundle: true,
+        platform: 'node',
+        define,
+        tsconfig: join(__dirname, '../tsconfig.main.json'),
+        plugins: [
+            esbuildDecorators({
+                tsconfig: join(__dirname, '../tsconfig.main.json')
+            })
+        ],
+        external: [
+            ...builtinModules.filter(x => !/^_|^(internal|v8|node-inspect)\/|\//.test(x)),
+            'electron'
+        ]
+    };
+};
---
+++ src/render/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script setup lang="ts">
+import { EVENTS } from "@common/events";
+import { useIpc } from "@render/plugins/ipc";
+import { ref } from "vue";
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: "Vite + Electron & Esbuild"
+    }
+});
+
+</script>
+
+<style>
+</style>
